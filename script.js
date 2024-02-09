c6e98c1b5eb95c672ea2180592bf0f6ffb4dcebb
JayaprakashPanabaka <jpanabaka1729@gmail.com>
2024-02-09 15:06:46 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let toggleBtn = document.getElementById('toggleBtn')
+let navLinks = document.getElementById('navLinks')
+
+toggleBtn.addEventListener('click', () => {
+    navLinks.classList.toggle('show-links')
+
+})
